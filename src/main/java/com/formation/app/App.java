c4b3cd48395231c99ece23c725454edb1b7be607
Bkake <bangaly.kake@gmail.com>
2018-09-19 12:10:32 +0200

    first commit

---
+++ src/main/java/com/formation/app/App.java
@@ -0,0 +1,15 @@
+package com.formation.app;
+
+public class App {
+    private final String message = "Hello World!";
+
+    public App() { }
+
+    public static void main(final String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private String getMessage() {
+        return message;
+    }
+}
